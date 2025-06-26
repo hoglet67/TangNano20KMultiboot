30ed265a6f0aa772b210ac20c1b7096eb658552f
David Banks <dave@hoglet.com>
2025-06-26 16:29:28 +0100

    Minor changes to release script

--- release.sh
+++ release.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# Kill child processes on ^C
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
+# Before doing anthing, check gw_sh is available
+type gw_sh >/dev/null 2>&1
+if [[ "$?" != "0" ]]; then
+    echo "Gowin gw_sh not found on the PATH" >&2
+    exit 1
+fi
+
+# Time the build
+start=$(date +'%s')
 
 flavours=(
     tang20k_nodebugger_pitube
@@ -21,7 +32,11 @@ done
 echo "Waiting for background tasks to complete"...
 wait $(jobs -p)
 
+
 cd build
 release=$(date +"%Y%m%d")_$(git rev-parse --short=8 HEAD)
 zip -r ../releases/${release}.zip */multiboot_cores_and_roms.bin
 cd ..
+
+end=$(date +'%s')
+echo "Build time: $(($end - $start)) seconds"
